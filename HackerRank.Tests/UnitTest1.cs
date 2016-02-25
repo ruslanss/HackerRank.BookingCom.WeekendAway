ffacd8aa9baba210a672ebf674ea13f69233bb9b
Chuck Norris <ruslanss@gmail.com>
2016-02-25 18:05:12 +0000

    before refactoring

---
+++ HackerRank.Tests/UnitTest1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HackerRank.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            /*
+1
+4 6
+1 2 2
+1 3 4
+1 4 8
+2 3 3
+2 4 3
+3 4 1
+*/
+            Tuple<int, int, int>[] input = new Tuple<int, int, int>[6];
+
+            input[0] = new Tuple<int, int, int>(1, 2, 2);
+            input[1] = new Tuple<int, int, int>(1, 3, 4);
+            input[2] = new Tuple<int, int, int>(1, 4, 8);
+            input[3] = new Tuple<int, int, int>(2, 3, 3);
+            input[4] = new Tuple<int, int, int>(2, 4, 3);
+            input[5] = new Tuple<int, int, int>(3, 4, 1);
+          
+
+            var result = Solution.MinDistance(input);
+
+            Assert.AreEqual(4, result);
+        }
+
+
+        [TestMethod]
+        public void SquareTest()
+        {
+            /*
+1
+6 7
+
+1 2 1
+2 3 1
+3 4 1
+4 1 1
+
+1 5 2
+5 6 2
+6 1 1
+
+*/
+            Tuple<int, int, int>[] input = new Tuple<int, int, int>[7];
+
+            input[0] = new Tuple<int, int, int>(1,2,1);
+            input[1] = new Tuple<int, int, int>(2,3,1);
+            input[2] = new Tuple<int, int, int>(3,4,1);
+            input[3] = new Tuple<int, int, int>(4,1,1);
+
+            input[4] = new Tuple<int, int, int>(1,5,2);
+            input[5] = new Tuple<int, int, int>(5,6,2);
+            input[6] = new Tuple<int, int, int>(6,1,1);
+
+
+            var result = Solution.MinDistance(input);
+
+            Assert.AreEqual(2, result);
+        }
+    }
+}
---
+++ HackerRank/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HackerRank
+{
+    public  class Solution
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+            var line2 = Console.ReadLine().Split(' ');
+            var numberOfRoads = Convert.ToInt32(line2[1]);
+            var numberOfLocations = Convert.ToInt32(line2[0]);
+
+            var locations = new List<int>[numberOfLocations]; //  locations = dict()
+
+            for (int i = 0; i < numberOfRoads; i++) // for m in range(0, M):
+            {
+                var ABD = Console.ReadLine().Split(' '); //  ABD = sys.stdin.readline().split()
+                var distance = Convert.ToInt32(ABD[2]);  //distance = int(ABD[2])
+                var A = Convert.ToInt32(ABD[0]) - 1;     // A = int(ABD[0])
+                var B = Convert.ToInt32(ABD[1]) - 1;     //B = int(ABD[1])
+                if (locations[A] == null)    //if A not in locations:
+                    locations[A] = new List<int>(); //    locations[A] = list()
+                if (locations[B] == null)    //if B not in locations:
+                    locations[B] = new List<int>();//    locations[B] = list()
+                locations[A].Add(distance); //locations[A].append(distance)
+                locations[B].Add(distance); //locations[B].append(distance)
+            }
+
+            long minimal_result = Int64.MaxValue; //minimal_result = float("inf")
+            for (int i = 0; i < locations.Length; i++) //for l in locations.values():
+            {                                          //    # no need to sort completely, only O(N)
+                var dist_smallest = Int32.MaxValue;    //    dist_smallest = float("inf")
+                var dist_small = Int32.MaxValue;       //    dist_small = float("inf")
+                foreach (var dist in locations[i])      //    for dist in l:
+                {
+                    if (dist < dist_smallest)   //        if dist < dist_smallest:
+                    {
+                        dist_small = dist_smallest; //            dist_small = dist_smallest
+                        dist_smallest = dist;        //            dist_smallest = dist
+                    }
+                    else if (dist < dist_small) //        elif dist < dist_small:
+                    {
+                        dist_small = dist;  //            dist_small = dist
+                    }
+                 }
+
+                long dist_total = dist_small + dist_smallest; //  dist_total = dist_small + dist_smallest
+                if (dist_total < minimal_result)            //    if dist_total < minimal_result:
+                    minimal_result = dist_total;            //        minimal_result = dist_total
+
+            }
+
+
+            Console.WriteLine(minimal_result);
+        }
+
+        public static int MinDistance(Tuple<int, int, int>[] locations)
+        {
+            var min = 101;
+
+            for (int i = 0; i < locations.Length; i++)
+            {
+                var pointA = locations[i].Item1;
+                var pointB = locations[i].Item2;
+                var distance = locations[i].Item3;
+
+                distance = distance + GetNearestPointC(pointA, pointB, locations);
+
+                if (min > distance)
+                    min = distance;
+            }
+
+            return min;
+        }
+
+        private static int GetNearestPointC(int pointA, int pointB, Tuple<int, int, int>[] locations)
+        {
+            var min = 101;
+            for (int i = 0; i < locations.Length; i++)
+            {
+                if (locations[i].Item1 == pointA && locations[i].Item2 == pointB)
+                    continue;
+                if (locations[i].Item1 == pointB && locations[i].Item2 == pointA)
+                    continue;
+
+                if (locations[i].Item1 != pointA && locations[i].Item2 != pointA && locations[i].Item1 != pointB &&
+                    locations[i].Item2 != pointB)
+                    continue;
+
+                if (min > locations[i].Item3)   min = locations[i].Item3;
+            }
+
+            return min;
+        }
+    }
+}
